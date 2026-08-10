01af664f7a17c6245a7d07013d59cfcd45959354
DrMarcusViniciusNicolas <marcus.viniciussn@hotmail.com>
2026-08-10 20:19:43 -0300

    Add files via upload

---
+++ site-marcus-nicolas-completo/config.js
@@ -0,0 +1,8 @@
+// Edite somente este arquivo quando quiser atualizar links e contato.
+const SITE_CONFIG = {
+  region: "São João da Boa Vista e região",
+  linkedin: "https://www.linkedin.com/in/drmarcusnicolas/",
+  email: "",
+  whatsapp: "",
+  lattes: ""
+};
