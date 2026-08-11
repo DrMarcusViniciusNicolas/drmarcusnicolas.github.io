4f7309bbe23a44dfa01c296e79ad519b62314b77
DrMarcusViniciusNicolas <marcus.viniciussn@hotmail.com>
2026-08-11 09:02:01 -0300

    Delete site-marcus-nicolas-completo directory

--- site-marcus-nicolas-completo/config.js
+++
@@ -1,8 +0,0 @@
-// Edite somente este arquivo quando quiser atualizar links e contato.
-const SITE_CONFIG = {
-  region: "São João da Boa Vista e região",
-  linkedin: "https://www.linkedin.com/in/drmarcusnicolas/",
-  email: "",
-  whatsapp: "",
-  lattes: ""
-};
